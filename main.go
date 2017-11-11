255a47c8e235ec5acdc631a3a3486674ba17797e
Brian Starkey <stark3y@gmail.com>
2017-11-11 19:33:48 +0000

    main: Hack together motor control
    Just sweep PWM direction, duty and frequency.

--- main.go
+++ main.go
@@ -1,23 +1,79 @@
 package main
 
 import (
-	"log"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"time"
+
+	"github.com/usedbytes/bot_matrix/datalink/connection"
 	"github.com/usedbytes/bot_matrix/datalink/packet"
 	"github.com/usedbytes/bot_matrix/datalink/spiconn"
 )
 
+func ledOn(c *connection.Connection) {
+	data := []packet.Packet{
+		{ 1, []byte{1} },
+	}
+	c.Transact(data)
+}
+
+func ledOff(c *connection.Connection) {
+	data := []packet.Packet{
+		{ 1, []byte{0} },
+	}
+	c.Transact(data)
+}
+
+func setFreq(c *connection.Connection, freq uint32) {
+	data := []packet.Packet{
+		{ Endpoint: 2, },
+	}
+
+	buf := &bytes.Buffer{}
+	binary.Write(buf, binary.LittleEndian, freq)
+
+	data[0].Data = buf.Bytes()
+
+	c.Transact(data)
+}
+
+func setDuty(c *connection.Connection, ch byte, dir byte, duty uint16) {
+	data := []packet.Packet{
+		{ Endpoint: 3, },
+	}
+
+	buf := &bytes.Buffer{}
+	binary.Write(buf, binary.LittleEndian, ch)
+	binary.Write(buf, binary.LittleEndian, dir)
+	binary.Write(buf, binary.LittleEndian, duty)
+
+	data[0].Data = buf.Bytes()
+
+	c.Transact(data)
+}
+
 func main() {
+	var on bool
 	c, _ := spiconn.NewSPIConn("/dev/spidev0.0")
 
-	p := packet.Packet{
-		Endpoint: 0x37,
-		Data: []byte{0x0a, 0x0b, 0x0c, 0x0d, },
-	}
+	for {
+		for f := uint32(2000); f < 20000; f += 1000 {
+			setFreq(c, f);
+			for rev := byte(0); rev <= 1; rev++ {
+				fmt.Printf("rev: %d\n", rev)
+				for d := uint16(0); d < 65535 - 300; d+=300 {
+					setDuty(c, 0, rev, d)
+					time.Sleep(30 * time.Millisecond)
+				}
+			}
 
-	s, err := c.Transact([]packet.Packet{p})
-	if err != nil {
-		log.Println(err)
+			if on {
+				ledOn(c)
+			} else {
+				ledOff(c)
+			}
+			on = !on
+		}
 	}
-
-	log.Printf("%#v\n", s)
 }
