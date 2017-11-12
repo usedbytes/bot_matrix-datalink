3054cd20fde3c5d6807466053781dc4d838638d4
Brian Starkey <stark3y@gmail.com>
2017-11-12 22:22:31 +0000

    main: Implement interactive shell with abiosoft/ishell

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"strconv"
 	"time"
 
+	"github.com/abiosoft/ishell"
 	"github.com/usedbytes/bot_matrix/datalink/connection"
 	"github.com/usedbytes/bot_matrix/datalink/packet"
 	"github.com/usedbytes/bot_matrix/datalink/spiconn"
@@ -101,7 +103,86 @@ func setIlimit(c *connection.Connection, il uint32) {
 func main() {
 	var on bool
 	c, _ := spiconn.NewSPIConn("/dev/spidev0.0")
-	fmt.Println("Hello")
+
+	// create new shell.
+	// by default, new shell includes 'exit', 'help' and 'clear' commands.
+	shell := ishell.New()
+
+	// display welcome info.
+	shell.Println("Driver...")
+
+	// register a function for "greet" command.
+	shell.AddCmd(&ishell.Cmd{
+		Name: "sp",
+		Help: "set_point",
+		Func: func(ctx *ishell.Context) {
+			if len(ctx.Args) != 1 {
+				ctx.Err(fmt.Errorf("Expected one argument (setpoint uint32)"))
+				return
+			}
+
+			sp, err := strconv.ParseUint(ctx.Args[0], 0, 32)
+			if err != nil {
+				ctx.Err(err)
+				return
+			}
+
+			setPoint(c, uint32(sp))
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "il",
+		Help: "ilimit",
+		Func: func(ctx *ishell.Context) {
+			if len(ctx.Args) != 1 {
+				ctx.Err(fmt.Errorf("Expected one argument (ilimi int32)"))
+				return
+			}
+
+			il, err := strconv.ParseUint(ctx.Args[0], 0, 32)
+			if err != nil {
+				ctx.Err(err)
+				return
+			}
+
+			setIlimit(c, uint32(il))
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "g",
+		Help: "gains",
+		Func: func(ctx *ishell.Context) {
+			if len(ctx.Args) != 3 {
+				ctx.Err(fmt.Errorf("Expected three arguments (Kc, Kd, Ki)"))
+				return
+			}
+
+			Kc, err := strconv.ParseFloat(ctx.Args[0], 64)
+			if err != nil {
+				ctx.Err(err)
+				return
+			}
+
+			Kd, err := strconv.ParseFloat(ctx.Args[1], 64)
+			if err != nil {
+				ctx.Err(err)
+				return
+			}
+
+			Ki, err := strconv.ParseFloat(ctx.Args[2], 64)
+			if err != nil {
+				ctx.Err(err)
+				return
+			}
+
+			setGains(c, Kc, Kd, Ki)
+		},
+	})
+
+	// run shell
+	shell.Run()
 
 	for {
 		for f := uint32(2000); f < 20000; f += 1000 {
