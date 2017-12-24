5c35cc6b5fd1e5a6dea55fec9607be0921e4ae7e
Brian Starkey <stark3y@gmail.com>
2017-12-24 00:46:12 +0000

    datalink: Remove Connection
    The abstraction was pretty broken - drop the
    Connection/Protocol/Transport interfaces. If we come up with a new use
    for them they're easy to add back.

--- datalink.go
+++ datalink.go
@@ -9,37 +9,3 @@ type Packet struct {
 type Transactor interface {
 	Transact([]Packet) ([]Packet, error)
 }
-
-type Protocol interface {
-	Serialise([]Packet) []byte
-	DeSerialise([]byte) ([]Packet, error)
-}
-
-type Transport interface {
-	Transfer([]byte) ([]byte, error)
-}
-
-type Connection struct {
-	protocol Protocol
-	transport Transport
-}
-
-func NewConnection(proto Protocol, xport Transport) *Connection {
-	return &Connection{ proto, xport }
-}
-
-func (c *Connection) Transact(packets []Packet) ([]Packet, error) {
-	tx := c.protocol.Serialise(packets)
-
-	rx, err := c.transport.Transfer(tx)
-	if err != nil {
-		return nil, err
-	}
-
-	rxPkts, err := c.protocol.DeSerialise(rx)
-	if err != nil {
-		return nil, err
-	}
-
-	return rxPkts, nil
-}
